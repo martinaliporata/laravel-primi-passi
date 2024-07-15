8a99ce318c66720923b7349851e645dab7849f2e
martinaliporata <martinaliporata@gmail.com>
2024-07-15 17:42:54 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+// quando l'utente chiama home...
+Route::get('/home', function() {
+    return view('home');
+});
+
+// quando l'utente chiama profile...
+Route::get('/profile', function() {
+    $name = "Martina";
+    $surname = "Liporata";
+    $age = 23;
+    $place = "Milano";
+    return view('profile', compact('name', 'surname', 'age', 'place'));
+});
